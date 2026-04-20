15970bbb383917737cfe13bcfb89aa2ba93f92f5
Mildret-Ventura <20243ds047@utez.edu.mx>
2026-04-20 06:51:41 -0600

    Delete EventNodeDiego/EventNodeDiego directory

--- EventNodeDiego/EventNodeDiego/backend/eventnode-api/src/main/java/com/eventnode/eventnodeapi/repositories/AdministradorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eventnode.eventnodeapi.repositories;
-
-import com.eventnode.eventnodeapi.models.Administrador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/** Persistencia de {@link com.eventnode.eventnodeapi.models.Administrador}. */
-public interface AdministradorRepository extends JpaRepository<Administrador, Integer> {
-}
--- EventNodeDiego/EventNodeDiego/web-app/src/services/categoriaService.js
+++
@@ -1,8 +0,0 @@
-import { apiGet, apiPost, apiPut, apiDelete } from './apiHelper'
-
-export const categoriaService = {
-  listar: () => apiGet('/categorias'),
-  crear: (nombre) => apiPost('/categorias', { nombre }),
-  actualizar: (id, nombre) => apiPut(`/categorias/${id}`, { nombre }),
-  eliminar: (id) => apiDelete(`/categorias/${id}`),
-}
